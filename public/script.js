d2c632acc23a7191d0c2321837d417f0ed1f6e56
Edward Zhou <esz7923@nyu.edu>
2023-10-29 19:32:34 -0400

    added final documentation

--- public/script.js
+++ public/script.js
@@ -9,15 +9,24 @@ window.onload = () => {
 
     let introSketch, callSketch;
 
+    // opening animation delays
     setTimeout(loginFade, 250);
     setTimeout(loginFade2, 1000);
+
+    // determines random color on load
     var color = Math.floor(Math.random() * 360);
 
     let video = document.getElementById('myvideo');
     let constraints = { audio: true, video: true };
+
     initializeControls();
 
+    /**
+     * Adds permanent event listeners to buttons and other elements
+     */
     function initializeControls() {
+
+      // event listener for joining room
       button.addEventListener('click', function() {
         let textField = document.getElementById('login-name');
         let roomField = document.getElementById('login-room');
@@ -25,19 +34,25 @@ window.onload = () => {
         let name = textField.value;
         let room = roomField.value;
 
+        // alerts if no name or room has been entered
         if(!name || !room) {
             alert('Please enter a name and room');
         } else {
             let introSketchElement = document.getElementsByClassName('p5Canvas')[0]
+
+            // zooms in and out of intro screen, fades out of intro sketch
             container.style.animation = "expand 1.5s ease forwards";
             introSketchElement.style.zIndex = -1;
             introSketchElement.style.animation = "fade 1.5s ease forwards";
+
+            // removes intro elements
             setTimeout(()=> {
                 introSketch.remove();
                 delete introSketch;
                 roomField.value = '';
             }, 1500);
 
+            // initializes WebRTC stuff and colors
             initializeCall(name, color, room);
             initializeControlColors(color, room);
         }
@@ -50,34 +65,36 @@ window.onload = () => {
 
       let brushControls = document.getElementById('brush-controls');
 
+      // toggle mute
       muteBtn.addEventListener('click', ()=> {
         muteBtn.firstChild.classList.toggle('fa-microphone');
         muteBtn.firstChild.classList.toggle('fa-microphone-slash');
         
         if(muteBtn.firstChild.classList.contains('fa-microphone')) {
-          socket.emit('user-mute', false);
+          socket.emit('user-mute', false); // unmute
         } else {
-          socket.emit('user-mute', true);
+          socket.emit('user-mute', true); // mute
         }
       }) 
 
+      // toggle different tools
       toolBtn.addEventListener('click', ()=> {
         toolBtn.firstChild.classList.toggle('fa-hand-pointer');
         toolBtn.firstChild.classList.toggle('fa-pencil');
         
         if(toolBtn.firstChild.classList.contains('fa-hand-pointer')) {
-          callSketch.controlling = true;
-          brushControls.style.animation = 'shrink 0.6s ease forwards';
+          callSketch.controlling = true; // controlling mode
+          brushControls.style.animation = 'shrink 0.6s ease forwards'; // disappear brush controls
           
         } else {
-          callSketch.controlling = false;
-          brushControls.style.animation = 'popout 0.6s ease forwards';
-          // draw mode
+          callSketch.controlling = false; // draw mode
+          brushControls.style.animation = 'popout 0.6s ease forwards'; // appear brush controls
         }
       }) 
 
+      // leave call event listener
       leaveBtn.addEventListener('click', ()=> {
-        document.body.style.animation = "fade 1s ease forwards";
+        document.body.style.animation = "fade 1s ease forwards"; // fades out of body screen
         setTimeout(leaveCall, 1000);
         setTimeout(loginFade, 1250);
         setTimeout(loginFade2, 1500);
@@ -86,11 +103,13 @@ window.onload = () => {
       let brushSizeSlider = document.getElementById('brush-size');
       let brushColSlider = document.getElementById('brush-col');
 
+      // updates brush size upon user input, changes slider knob
       brushSizeSlider.addEventListener('input', ()=> {
         brushSizeSlider.style.setProperty('--sliderSize', brushSizeSlider.value + "px")
         callSketch.brushSize = brushSizeSlider.value;
       })
 
+      // updates brush color upon user input, changes slider knob
       brushColSlider.addEventListener('input', ()=> {
         brushColSlider.style.setProperty('--thumbColor', "hsl(" + color + "deg, 100%, " + (100-brushColSlider.value) + "%)")
         callSketch.brushCol = (100-brushColSlider.value);
@@ -103,6 +122,7 @@ window.onload = () => {
       infoIcon.style.opacity = 1;
       infoParagraph.style.opacity = 0;
 
+      // info popup interaction
       infoBtn.addEventListener('mouseenter', ()=> {
         infoBtn.style.width = '30vh';
         infoBtn.style.height = '40vh';
@@ -118,15 +138,25 @@ window.onload = () => {
         infoParagraph.style.opacity = 0;
       })
     }
+
+    /**
+     * Initializes colors of UI based on random color, shows controls
+     * 
+     * @param {Number} color Hue value of HSB color
+     * @param {String} room room key
+     */
     function initializeControlColors(color, room) {
+
+      // updates brush control colors
       document.getElementById('brush-controls').style.backgroundColor = "hsl(" + color + "deg, 100%, 70%)"
       for(let slider of document.getElementsByClassName('slider')) {
         slider.style.setProperty('--thumbColor', "hsl(" + color + "deg, 100%, 20%)")
         slider.style.setProperty('--thumbOutline', "hsl(" + color + "deg, 100%, 20%)")
       }
 
-      document.getElementById('controls').style.display = 'flex';
+      document.getElementById('controls').style.display = 'flex'; // shows call controls
 
+      // updates call control colors
       for(let btn of document.getElementsByClassName('control-btn')) {
         btn.style.animation = 'popout 0.8s ease 1s forwards';
         btn.style.backgroundColor = "hsl(" + color + "deg, 100%, 90%)";
@@ -146,6 +176,7 @@ window.onload = () => {
       let infoBtn = document.getElementById('controls-info');
       let roomKeyDisp = document.getElementById('room-key-display');
 
+      // updates info container colors and shows
       infoContainer.style.animation = 'fade 0.8s ease 1.5s reverse forwards';
       infoBtn.style.backgroundColor = "hsl(" + color + "deg, 100%, 20%)";
       infoBtn.style.color = "hsl(" + color + "deg, 100%, 90%)";
@@ -153,6 +184,10 @@ window.onload = () => {
       roomKeyDisp.textContent = "Room: " + room;
 
     }
+
+    /**
+     * Initializes colors of intro UI elements and shows
+     */
     function loginFade() {
       let title = document.getElementById("title");
       let prompt = document.getElementById("prompt");
@@ -163,7 +198,7 @@ window.onload = () => {
       prompt.style.color = "hsl(" + color + "deg, 100%, 20%)";
       container.style.backgroundColor = "hsl(" + color + "deg, 100%, 90%)";
 
-      
+      // submit button
       button.addEventListener('mouseenter', ()=> {
           button.style.backgroundColor = "hsl(" + color + "deg, 100%, 20%)";
           button.style.color = "hsl(" + color + "deg, 100%, 95%)";
@@ -177,10 +212,13 @@ window.onload = () => {
       container.style.transform = "scale(1)";
     }
 
+    /**
+     * Initializes colors of intro UI elements and shows, creates intro sketch
+     */
     function loginFade2() {
       let nontitleContainer = document.getElementById('non-title-content');
       let opacity = 0;
-
+      // fades in interactive elements
       let infoFadeIn = setInterval(()=> {
           if(opacity >= 1) {
               clearInterval(infoFadeIn);
@@ -191,17 +229,21 @@ window.onload = () => {
       introSketch = new p5(defIntroSketch(color));
       
     }
+
+    /**
+     * User leaves call, resets UI animations and displays
+     */
     function leaveCall() {
-      socket.disconnect();
+      socket.disconnect(); // disconnects from socket
       socket = null;
 
+      // removes peers
       for(let sid in simplepeers) {
         simplepeers[sid].simplepeer.destroy();
-        // document.getElementById(sid).remove();
-        // document.getElementById(sid + "-canvas").remove();
         delete simplepeers[sid];
       }
 
+      // resets call UI elements
       document.body.style.animation = 'none';
       document.getElementById('controls').style.display = 'none';
       document.getElementById('usercanvas').style.display = 'none';
@@ -209,6 +251,7 @@ window.onload = () => {
         btn.style.animation = 'none';
       }
 
+      // resets call UI properties
       if(!callSketch.controlling) {
         callSketch.controlling = true;
         document.getElementById('brush-controls').style.animation = 'none';
@@ -224,11 +267,21 @@ window.onload = () => {
       container.style.transform = "scale(0)";
       document.getElementById('non-title-content').style.opacity = 0;
 
+      // removes call sketch
       callSketch.remove();
       callSketch = null;
 
+      // resets color
       color = Math.floor(Math.random() * 360);
     }
+
+    /**
+     * Initializes simplepeers, socketio, and call sketch, sets up user media
+     * 
+     * @param {String} name User name
+     * @param {Number} color Hue of HSB color
+     * @param {String} room room key
+     */
     function initializeCall(name, color, room) {
         navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
             /* Use the stream */
@@ -243,8 +296,10 @@ window.onload = () => {
               draw();
             };
           
+            // sets up socket
             setupSocket(room);
-            console.log(name);
+
+            // Creates new call sketch, sets default values
             callSketch = new p5(defCallSketch(name, color));
             callSketch.controlling = true;
             callSketch.brushSize = 20;
@@ -259,12 +314,16 @@ window.onload = () => {
             alert(err);
           });
     }
+
+    /**
+     * Draws circle overlay over user video
+     */
     function draw() {
         let canvas = document.getElementById('usercanvas');
         let context = canvas.getContext("2d");
-        context.globalCompositeOperation='source-over';
+        context.globalCompositeOperation='source-over'; // draws base image
         context.drawImage(video,0,0);
-        context.globalCompositeOperation='destination-in';
+        context.globalCompositeOperation='destination-in'; // draws mask
         
         context.beginPath();
         context.arc(canvas.width/2,canvas.height/2,canvas.height/2,0,Math.PI*2);
@@ -275,31 +334,44 @@ window.onload = () => {
     }
 
 }
-  
+
+/**
+ * Helper function to connect to socket and wrap socket functions
+ * @param {String} room room key
+ */
 function setupSocket(room) {
   
+    // connects to socket and immediately joins room
     socket = io.connect();
     socket.emit('join-room', room);
 
+    /**
+     * Confirms connection
+     */
     socket.on('connect', function () {
       console.log("Connected As", socket.id);
       console.log('joining room ' + room);
     });  
   
+    /**
+     * Removes simplepeer upon peer disconnect
+     */
     socket.on('peer-disconnect', function(data) {
-      console.log("simplepeer has disconnected " + data);
+
+      // finds simplepeer
       for (let sid in simplepeers) {
         if (simplepeers[sid].socket_id == data) {
-          console.log("Removing simplepeer: " + sid);
           delete simplepeers[sid];
-          // Should also remove video from page
+          // Removes video and canvas elements form page
           document.getElementById(data).remove();
           document.getElementById(data + "-canvas").remove();
         } 
       }			
     });	
   
-  
+    /**
+     * Gets list of socket ids from server to connect to via SimplePeers
+     */
     socket.on('listresults', (data) => {
       for (let sid of data) {
         if (sid != socket.id) {
@@ -309,11 +381,17 @@ function setupSocket(room) {
       }
     });
   
+    /**
+     * Mutes/unmutes video based on specified socket id and value
+     */
     socket.on('user-mute', (data) => {
       let video = document.getElementById(data.sid);
       video.muted = data.val;
     })
   
+    /**
+     * Communicates with peers using server as medium
+     */
     socket.on('signal', (to, from, data) => {
       console.log("Got a signal from the server: ", to, from, data);
       if (to != socket.id) {
@@ -334,6 +412,7 @@ function setupSocket(room) {
 }
   
 // A wrapper for simplepeer as we need a bit more than it provides
+// Mostly written by Shawn Van Every
 class SimplePeerWrapper {
   
     constructor(initiator, socket_id, socket, stream) {
@@ -388,6 +467,13 @@ class SimplePeerWrapper {
           draw(canvas.id, ovideo);
         };
   
+        /**
+         * Draws circle overlay over video
+         * 
+         * @param {String} canvasid id of HTML canvas object
+         * @param {Object} video HTML video object
+         * @returns 
+         */
         function draw(canvasid, video) {
           let canvas = document.getElementById(canvasid);
           if(!canvas) return;
@@ -414,15 +500,15 @@ class SimplePeerWrapper {
   
   
         document.body.appendChild(ovideo);
-        console.log(ovideo);
       });
   
       this.simplepeer.on('close', () => {
         console.log('Got close event');
-        // Should probably remove from the array of simplepeers
+        // Remove from the array of simplepeers
         let video = document.getElementById(this.socket_id);
         let canvas = document.getElementById(this.socket_id + "-canvas");
 
+        // removes video and canvas from page
         if(video) 
           video.remove();
         if(canvas)
